b97d2d133dd8cffdf0dc2520fd827d62f405b57e
Alexander Rubin <your@email.here>
2024-10-07 16:39:54 -0500

    build got access token

--- api/main.py
+++ api/main.py
@@ -23,3 +23,25 @@ app.include_router(routers_sales.router, tags=["SALES"])
 app.include_router(routers_appointments.router, tags=["APPOINTMENTS"])
 app.include_router(routers_security.router, tags=["SECURITY"])
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
--- simpl-frontend/src/components/Login.jsx
+++ simpl-frontend/src/components/Login.jsx
@@ -1,9 +1,102 @@
-const Login = () => {
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
+const Login = () => {
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    // useEffect (() => {
+    //     try {
+    //         const loginResponse = axios.post('http://localhost:8000/token', {
+    //             username: username,
+    //             password: password
+    //         },
+    //         {
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         }
+    //     )
+    //         console.log(loginResponse.data)
+
+    //     } catch (error) {
+    //     }
+    // }, [username, password])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const { data: loginResponse } = await axios.post(
+                'http://localhost:8000/token', 
+                new URLSearchParams({
+                    grant_type: 'password',
+                    username: username,
+                    password: password,
+                    scope: '',
+                    client_id: '',
+                    client_secret: ''
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        )
+        console.log(loginResponse)
+        } catch (error) {
+            console.error('login failed', error)
+        }
+    }
+
+    const handleUsernameChange = (e) => {
+        e.preventDefault()
+        console.log(e.target.value)
+        setUsername(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        e.preventDefault()
+        console.log(e.target.value)
+        setPassword(e.target.value)
+    }
 
     return (
         <>
-            <p>does this work?</p>
+            <div className="container-sm">
+                <form 
+                    className='form-control'
+                    onSubmit={handleSubmit}
+                >
+                    <label>
+                        <h3>
+                            login
+                        </h3>
+                    </label>
+                    <input 
+                        type="username" 
+                        onChange={handleUsernameChange}
+                        className="form-control" 
+                        id="username" 
+                        placeholder="username" />
+                    <input 
+                        type="password" 
+                        onChange={handlePasswordChange}
+                        className="form-control" 
+                        id="password" 
+                        placeholder="password" />
+                    <button 
+                        type="submit" 
+                        className='btn btn-success'>
+                            submit
+                    </button>
+                    <br />
+                    <button 
+                        type="link" 
+                        className='btn btn-link'>
+                            create account
+                    </button>
+                </form>
+            </div>
         </>
     )
 }
