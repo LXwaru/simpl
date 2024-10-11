0647e6aac2be84b47f6ef718da71e7a9be4f02f2
Alexander Rubin <your@email.here>
2024-10-11 16:42:33 -0500

    setup redux-persist

--- simpl-frontend/src/app/store.js
+++ simpl-frontend/src/app/store.js
@@ -1,9 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../features/user/userSlice'
+import {configureStore} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'
+import {combineReducers} from "redux"; 
+import { persistReducer, persistStore } from 'redux-persist'
+import { thunk } from 'redux-thunk';
+import userSlice from '../features/user/userSlice';
+
+const reducers = combineReducers({
+    user: userSlice
+});
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 
-export default configureStore({
-    reducer: {
-        user: userReducer
-    }
-})
+const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false, // This avoids errors with non-serializable data in the state
+        }).concat(thunk), // Adds thunk middleware
+});
+
+export const persistor = persistStore(store);
+
+export default store;
--- simpl-frontend/src/main.jsx
+++ simpl-frontend/src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import store from './app/store.js'
+import store, { persistor } from './app/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react' 
 import './index.css'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App /> 
+      <PersistGate loading={null} persistor={persistor}>
+          <App /> 
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
